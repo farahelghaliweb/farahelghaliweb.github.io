d058b4fd094606358713ad8591398410bdd3ca0b
farahelghaliweb <144857164+farahelghaliweb@users.noreply.github.com>
2023-09-14 17:11:21 -0400

    Add files via upload

---
+++ projetcoiffeur/index.php
@@ -0,0 +1,557 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>projet portfolio</title>
+
+	<!--boostrap -->
+
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css"/>
+
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+
+
+	<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+	<!-- SLICK SLIDER -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css" integrity="sha512-wR4oNhLBHf7smjy0K4oqzdWumd+r5/+6QO/vDda76MW5iug4PT7v86FoEkySIJft3XA0Ae6axhIvHrqwm793Nw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!--FANCYBOX -->
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css"/>
+
+
+
+	<!-- OWL SLIDER -->
+  
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+
+
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	<header>
+		<div class="container">
+			<nav class="nav-flex">
+				<div class="logo">
+					<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDX_EApe712Fs064XZbZhw18dUVTlsf--Tiw&usqp=CAU">
+				</div>
+
+			    <div class="burger">
+				    <i class="fa-solid fa-bars"></i>
+				</div>
+
+				<div class="menu-header">
+					<ul class="menu-liste">
+						<li>
+							<a href="">HOME</a>
+						</li>
+						<li>
+							<a href="">HAIRCUT</a>
+						</li>
+						<li>
+							<a href="">SERVICES</a>
+						</li>
+						<li>
+							<a href="">ABOUT</a>
+						</li>
+						<li>
+							<a href="">CONTACT</a>
+						</li>
+
+						<li>
+							<a href="">
+								<i class="fa-brands fa-facebook"></i>
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<i class="fa-brands fa-youtube"></i>
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<i class="fa-brands fa-instagram"></i>
+							</a>
+						</li>
+
+						<li>
+							<a href="">
+								<i class="fa-solid fa-phone"></i>
+							</a>
+						</li>
+						
+					</ul>
+				</div>
+			</nav>
+		</div>
+	</header>
+
+	<section class="banner">
+		<div class="textbox">
+			<h2>beautiful hair,
+			 <br>healthy you!
+			</h2>
+
+		    <div>
+				<a class="btn1" href="">BOOK NOW </a>
+		    </div>
+
+				
+		</div>
+		
+	</section>
+
+	<section class="about">
+		<div class="container">
+
+			<div class="parent-flex">
+
+
+				<div class="gauche"></div>
+				<div class="droite">
+					<h2>Welcome to	
+					<br>
+                    Hair Salon</h2>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt architecto ab hic rem placeat eius commodi eum eligendi recusandae sed qui cumque quibusdam.</p>
+					<a class="btn" href="">LIRE PLUS</a>
+				</div>
+
+			</div>
+
+
+		</div>
+	</section>
+
+
+
+
+
+
+	     <!--OWL-->
+
+	     <section class="collection" id="question7">
+
+	    <div class="container">
+	    
+	    <div class="home-demo">
+	    	<h1>notre services</h1>
+	      
+	 
+	  <div class="owl-carousel owl-theme">
+
+	    <div class="item">
+	      <img src="https://www.coiffdeal.fr/public/img/big/AdobeStock17050600611600x1200jpeg_5fdb899da4cc2.jpeg">
+	    </div>
+	    <div class="item">
+	      <img src="https://media.istockphoto.com/id/1186053060/fr/photo/les-mains-du-coiffeur-tiennent-les-cheveux-roux-dans-le-salon-de-beaut%C3%A9.jpg?s=612x612&w=0&k=20&c=Md4MzvOgoxi0HQkf4UIvm8YrrknCSbDDFkwaWyq8o8Q=">
+	    </div>
+	    <div class="item">
+	     <img src="https://cdn1.treatwell.net/images/view/v2.i1949764.w720.h480.x667F8DA8/">
+	    </div>
+	    <div class="item">
+	       <img src="https://us.123rf.com/450wm/oskanov/oskanov1807/oskanov180700039/104509902-la-maquilleuse-de-vue-arri%C3%A8re-maquille-la-mari%C3%A9e.jpg?ver=6">
+	    </div>
+	    <div class="item">
+	      <img src="https://www.iamlamode.com/wp-content/uploads/2021/10/Les-meilleurs-salons-de-coiffure-a-Dijon.jpg">
+	    </div>
+	     <div class="item">
+	      <img src="https://img.freepik.com/photos-premium/coiffeur-fait-elegante-coiffure-fleurs-blanches-dans-cheveux_118086-2710.jpg">
+	    </div>
+	    
+	   
+	   
+	  
+	  </div>
+	</div>
+	</div>
+	</section>
+
+	<section class="galery">
+		<div class="container">
+			<div class="section-header">
+
+				<h2>GALLERY</h2>
+				
+			</div>
+		    <div class="galery-flex">
+
+			 <a href="coif1.jpg" data-fancybox="coif" data-caption="ceci est les titre">
+				<img src="coif1.jpg">
+			 </a>
+
+			 <a href="coif2.jpg" data-fancybox="coif" data-caption="NOTRE TRAVAIL">
+				<img src="coif2.jpg">
+			  </a>
+			     
+
+
+			<a href="coif3.jpg" data-fancybox="coif" data-caption="NOTRE TRAVAIL">
+				<img src="coif3.jpg">
+			 </a>
+
+			<a href="coif4.jpg" data-fancybox="coif" data-caption="NOTRE TRAVAIL">
+				<img src="coif4.jpg">
+			    </a>    
+
+			<a href="coif5.jpg" data-fancybox="resto" data-caption="NOTRE TRAVAIL">
+				<img src="coif5.jpg">
+			</a>
+
+			<a href="coif6.jpg" data-fancybox="resto" data-caption="NOTRE TRAVAIL">
+				<img src="coif6.jpg">
+			</a>
+
+			<a href="coif7.jpg" data-fancybox="resto" data-caption="NOTRE TRAVAIL">
+				<img src="coif7.jpg">
+			</a>
+
+			<a href="coif8.jpg" data-fancybox="resto" data-caption="NOTRE TRAVAIL">
+				<img src="coif8.jpg">
+			</a>
+
+			<a href="coif9.jpg" data-fancybox="resto" data-caption="NOTRE TRAVAIL">
+				<img src="coif9.jpg">
+			</a>
+
+			<a href="coif10.jpg" data-fancybox="resto" data-caption="NOTRE TRAVAIL">
+				<img src="coif10.jpg">
+			</a>
+
+			<a href="coif11.jpg" data-fancybox="resto" data-caption="NOTRE TRAVAIL">
+				<img src="coif11.jpg">
+			</a>
+
+			<a href="coif12.jpg" data-fancybox="resto" data-caption="NOTRE TRAVAIL">
+				<img src="coif12.jpg">
+			</a>
+		    
+	    </div>  	
+	</section>
+
+		<section class="produit">
+		<div class="container">
+
+			<div class="parent-flex">
+
+
+				<div class="gauche-produit"></div>
+				<div class="droite-produit">
+					<h2>New hairstyle!</h2>
+					<hr>
+
+					<p>“Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique dolorem quisquam laudantium, incidunt id laborum, tempora aliquid labore minus. Nemo maxime, veniam! Fugiat odio nam eveniet ipsam atque, corrupti porro”
+						<br>
+					— Stellla Martin</p>
+					
+				</div>
+
+			</div>
+
+
+		</div>
+	</section>
+
+     <h1>NOTRE SERVICES </h1>
+	<section class="card-area">
+		
+
+	            <!-- Card: Coiffure -->
+	            <section class="card-section">
+	                <div class="card">
+	                    <div class="flip-card">
+	                        <div class="flip-card__container">
+	                            <div class="card-front">
+	                                <div class="card-front__tp card-front__tp--city">
+
+	                               <h2 class="card-front__heading">
+	                                coiffure
+	                            </h2>
+	                            <p class="card-front__text-price">
+	                                From 100$
+	                            </p>
+	                                </div>
+
+	                                <div class="card-front__bt">
+	                                    <p class="card-front__text-view card-front__text-view--city">
+	                                        View me
+	                                    </p>
+	                                </div>
+	                            </div>
+	                            <div class="card-back">
+	                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_Ko3si2yOiz7gnOkUvxxu8fY1tLEDctPmYU8vq0KFP8mv6Cf9yCRIepUpXfoPNMsWRe4&usqp=CAU" alt="">
+	                            </div>
+	                        </div>
+	                    </div>
+
+	                    <div class="inside-page">
+	                        <div class="inside-page__container">
+	                            <h3 class="inside-page__heading inside-page__heading--city">
+	                                For coiffure lovers
+	                            </h3>
+	                            <p class="inside-page__text">
+	                               As cities never sleep, there are always something going on, no matter what time!
+	                            </p>
+	                            <a href="#" class="">View deals</a>
+	                        </div>
+	                    </div>
+	                </div>
+	            </section>
+
+	            <!-- Card: balayage -->
+	            <section class="card-section">
+	                <div class="card">
+	                    <div class="flip-card">
+	                        <div class="flip-card__container">
+	                            <div class="card-front">
+	                                <div class="card-front__tp card-front__tp--ski">
+	                                    
+	                                               <h2 class="card-front__heading">
+	                                                   balayage
+	                                               </h2>
+	                                               <p class="card-front__text-price">
+	                                                   From 300$
+	                                               </p>
+	                                </div>
+
+	                                <div class="card-front__bt">
+	                                    <p class="card-front__text-view card-front__text-view--ski">
+	                                        View me
+	                                    </p>
+	                                </div>
+	                            </div>
+
+	                            <div class="card-back">
+	                               <img src="https://www.jennif-hairbeauty.fr/photos/crop200x200/flamboyage-colores-e-pelo-2021_c63dec3c_1280x1829-717x1024-.jpg" alt="">
+	                            </div>
+	                        </div>
+	                    </div>
+
+	                    <div class="inside-page">
+	                        <div class="inside-page__container">
+	                            <h3 class="inside-page__heading inside-page__heading--ski">
+	                                For snow lovers
+	                            </h3>
+	                            <p class="inside-page__text">
+	                               Love snow? Why not take up exciting ski-in sessions and hit the slope? 
+	                            </p>
+	                            <a href="#" class="">View deals</a>
+	                        </div>
+	                    </div>
+	                </div>
+	            </section>
+
+	            <!-- Card: coupe -->
+	            <section class="card-section">
+	                <div class="card">
+	                    <div class="flip-card">
+	                        <div class="flip-card__container">
+	                            <div class="card-front">
+	                                <div class="card-front__tp card-front__tp--beach">
+	                               
+	                                               <h2 class="card-front__heading">
+	                                                   coupe 
+	                                               </h2>
+	                                               <p class="card-front__text-price">
+	                                                   From 50$
+	                                               </p>
+	                                </div>
+
+	                                <div class="card-front__bt">
+	                                    <p class="card-front__text-view card-front__text-view--beach">
+	                                        View me
+	                                    </p>
+	                                </div>
+	                            </div>
+	                            <div class="card-back">
+	                                <img src="https://i.pinimg.com/200x/e1/94/c5/e194c53c9e04cfdc8051729bdcacbb99.jpg" alt="">
+	                            </div>
+	                        </div>
+	                    </div>
+
+	                    <div class="inside-page">
+	                        <div class="inside-page__container">
+	                            <h3 class="inside-page__heading inside-page__heading--beach">
+	                                For sun lovers
+	                            </h3>
+	                            <p class="inside-page__text">
+	                               Relax and get sun-kissed tan in the sea or take up surfting for an adventure!
+	                            </p>
+	                            <a href="#" class="">View deals</a>
+	                        </div>
+	                    </div>
+	                </div>
+	            </section>
+
+	            <!-- Card: coloration -->
+	            <section class="card-section">
+	                <div class="card">
+	                    <div class="flip-card">
+	                        <div class="flip-card__container">
+	                            <div class="card-front">
+	                                <div class="card-front__tp card-front__tp--camping">
+	                                               <h2 class="card-front__heading">
+	                                                   Coloration
+	                                               </h2>
+	                                               <p class="card-front__text-price">
+	                                                   From 150$
+	                                               </p>
+	                                </div>
+
+	                                <div class="card-front__bt">
+	                                    <p class="">
+	                                        View me
+	                                    </p>
+	                                </div>
+	                            </div>
+	                            <div class="card-back">
+	                                <img src="https://static1.puretrend.com/articles/3/97/43/3/@/2008512-blonde-brune-ou-rousse-les-femmes-au-200x200-1.jpg" alt="">
+	                            </div>
+	                        </div>
+	                    </div>
+
+	                    <div class="inside-page">
+	                        <div class="inside-page__container">
+	                            <h3 class="inside-page__heading inside-page__heading--camping">
+	                                For nature lovers
+	                            </h3>
+	                            <p class="inside-page__text">
+	                               Get your boots on for some hiking and explore all the beautiful scenery of nature!
+	                            </p>
+	                            <a href="#" class="">View deals</a>
+	                        </div>
+	                    </div>
+	                </div>
+	            </section>
+	        </section>
+
+	
+	<footer class="footer-distributed">
+
+	      <div class="footer-left">
+
+	        <div class="logo">
+					<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDX_EApe712Fs064XZbZhw18dUVTlsf--Tiw&usqp=CAU">
+				</div>
+
+	        <p class="footer-links">
+	          <a href="#" class="link-1">Home</a>
+	          
+	          <a href="#">haircut</a>
+	        
+	          <a href="#">services</a>
+	        
+	          <a href="#">About</a>
+	          
+	          <a href="#">Contact</a>
+	        </p>
+
+	        <p class="footer-company-name">salon de coiffure © 2023</p>
+	      </div>
+
+	      <div class="footer-center">
+
+	        <div>
+	          <i class="fa fa-map-marker"></i>
+	          <p><span>444 S. Cedros Ave</span> Solana Beach, California</p>
+	        </div>
+
+	        <div>
+	          <i class="fa fa-phone"></i>
+	          <p>+1.555.555.5555</p>
+	        </div>
+
+	        <div>
+	          <i class="fa fa-envelope"></i>
+	          <p><a href="mailto:support@company.com">salon@coiffure.com</a></p>
+	        </div>
+
+	      </div>
+
+	      <div class="footer-right">
+
+	        <p class="footer-company-about">
+	          <span>About the salon</span>
+	          Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
+	        </p>
+
+	        <div class="footer-icons">
+
+	          <a href="">
+				   <i class="fa-brands fa-facebook"></i>
+			  </a>
+
+			  <a href="">
+			  		<i class="fa-brands fa-youtube"></i>
+			  </a>
+			  
+			 
+			  	<a href="">
+			  		<i class="fa-brands fa-instagram"></i>
+			  	</a>
+			 
+
+			  
+			  	<a href="">
+			  		<i class="fa-solid fa-phone"></i>
+			  	</a>
+			  
+	          
+
+	        </div>
+
+	      </div>
+
+	    </footer>
+
+
+
+
+
+		<!-- JQUERY -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+		<script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.umd.js"></script>
+
+		 <!-- OWL SLIDER -->
+	  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+
+		<!-- SLICK SLIDER -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js" integrity="sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+		<!-- BOOTSTRAP -->
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+		<script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.umd.js"></script>
+
+		<!-- MON JAVASCRIPT -->
+		<script src="script.js"></script>
+
+
+
+	
+
+
+
+
+	
+</body>
+</html>
---
+++ projetcoiffeur/script.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+
+	$(function() {
+  // Owl Carousel
+  var owl = $(".owl-carousel");
+  owl.owlCarousel({
+    items: 3,
+    margin: 10,
+    loop: true,
+    nav: true
+      });
+       });
+
+
+
+	 Fancybox.bind();
+
+
+
+
+}); //effacez moi jamais
